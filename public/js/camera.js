20c62a2947115e7000d755a93e1b901e305bbb4f
udit034 <udit.mehata@gmail.com>
2019-02-27 22:56:33 -0800

    thirdpartyAPI lab

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,6 +1,6 @@
 function initCamera() {
   // Uncomment and fill in the correct selectors below.
-  capture($('#camera-vide'),
+  capture($('#camera-video'),
           $('#camera-canvas'),
           $('#camera-button'));
 
